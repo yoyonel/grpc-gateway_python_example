a48e6c9b1248b1b2d03ec1eebee36cb051fe2ebf
Lionel Atty <lionel.atty@365talents.com>
2020-01-15 17:23:41 +0100

    First commit.

---
+++ apt.sh
@@ -0,0 +1,7 @@
+
+sudo apt install -y \
+    libprotobuf-dev libprotoc-dev
+    # gogoprotobuf \
+    # golang-goprotobuf-dev \
+    # golang-github-grpc-ecosystem-grpc-gateway-dev \
+    # golang-grpc-gateway
